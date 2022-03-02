3130ac7e68abe92a9e8ce1afeb6b6f08236ae2c2
alagtari <alagtari@yahoo.fr>
2022-03-02 12:22:42 +0100

    test

--- src/main/java/com/example/filedemo/controller/RestHelloWorld.java
+++ src/main/java/com/example/filedemo/controller/RestHelloWorld.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RestHelloWorld {
 	
+	
 	@GetMapping("/test")
 	public String sayHello() {
 		return "Hello world! this is fgs";
